1e850e75d9aaf6216a8d8d1ca44a6db99211019f
juan <juancassiano@hotmail.com.br>
2022-01-06 12:53:26 -0300

    validaçao de cadastro

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,11 +5,24 @@ import { CategoryRepository } from "../repositories/CategoriesRepository";
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoryRepository();
 
+// Create Category
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExist = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExist) {
+    return response.status(400).json({ error: "Category already exist!" });
+  }
   categoriesRepository.create({ name, description });
   return response.status(201).send();
+});
+
+// List Category
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
