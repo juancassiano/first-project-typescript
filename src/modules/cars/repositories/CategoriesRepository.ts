eb6734248037b2147fcddda54d9826500484390b
juan <juancassiano@hotmail.com.br>
2022-01-09 15:35:53 -0300

    Using Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -4,13 +4,21 @@ import {
   ICreateCategoryDTO,
 } from "./ICategoriesRepository";
 
-// DTO -> Data Transfer Object
-
 class CategoriesRepository implements ICategoriesRepository {
-  private categories: Category[] = [];
+  private categories: Category[];
 
-  constructor() {
+  // eslint-disable-next-line no-use-before-define
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ description, name }: ICreateCategoryDTO): void {
